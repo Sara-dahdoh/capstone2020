a1984b55ad24a169944147ec57b67bc2b0897eaa
riyadgh <riyad.progs@gmail.com>
2020-03-13 19:39:32 -0400

    Refactor the test class, and raise exceptions from reggov_api_doc_error rather than catching too general exception Exception

--- src/c20_server/documents_ids.py
+++ src/c20_server/documents_ids.py
@@ -1,6 +1,7 @@
 # import os
 # from dotenv import load_dotenv
 import requests
+from c20_server import reggov_api_doc_error
 
 
 def get_documents_ids(api_key, docket_id):
@@ -8,13 +9,14 @@ def get_documents_ids(api_key, docket_id):
     url = "{}?api_key={}&dktid={}".format(api_url, api_key, docket_id)
 
     response = requests.get(url)
-    try:
-        if response.status_code == 200:
-            documents = response.json().get('documents')
-
-            documents_ids = [document['documentId'] for document in documents]
-            return documents_ids, 200
-    except Exception:
-        return [], response.status_code
-    else:
-        return [], response.status_code
+    if response.status_code == 403:
+        raise reggov_api_doc_error.IncorrectApiKeyException
+    if response.status_code == 404:
+        raise reggov_api_doc_error.BadDocIDException
+    if response.status_code == 400:
+        raise reggov_api_doc_error.IncorrectIDPatternException
+    if response.status_code == 429:
+        raise reggov_api_doc_error.ExceedCallLimitException
+    documents = response.json().get('documents')
+    documents_ids = [document['documentId'] for document in documents]
+    return documents_ids, 200
--- tests/test_get_documents_ids.py
+++ tests/test_get_documents_ids.py
@@ -1,7 +1,9 @@
 
 import os
+import pytest
+import requests_mock
 from dotenv import load_dotenv
-from c20_server import documents_ids
+from c20_server import documents_ids, reggov_api_doc_error
 # import requests
 # import pytest
 # import requests_mock
@@ -15,42 +17,39 @@ DKT_ID = "EPA-HQ-OAR-2011-0028"
 API_URL = "{}?api_key={}&dktid={}".format(URL, API_KEY, DKT_ID)
 
 
-def test_get_response(requests_mock):
+def test_get_response():
     return_json = {'documents': [{'documentId': 'EPA-HQ-OAR-2011-0028-0041'}]}
-    requests_mock.get(API_URL, json=return_json, status_code=200)
+    with requests_mock.Mocker() as mock:
+        mock.get(API_URL, json=return_json, status_code=200)
 
-    # getting list of documents IDS
-    resp = documents_ids.get_documents_ids(API_KEY, DKT_ID)
+        # getting list of documents IDS
+        resp = documents_ids.get_documents_ids(API_KEY, DKT_ID)
+        assert resp[0] == ['EPA-HQ-OAR-2011-0028-0041']
 
-    assert resp[0] == ['EPA-HQ-OAR-2011-0028-0041']
 
-
-def test_bad_docket_id(requests_mock):
+def test_bad_docket_id():
     dktid2 = 'EPA-HQ-0000'
     api_url1 = "{}?api_key={}&dktid={}".format(URL, API_KEY, dktid2)
-    requests_mock.get(api_url1, json=[], status_code=200)
-
     # getting empty data list
-    resp = documents_ids.get_documents_ids(API_KEY, dktid2)
+    with requests_mock.Mocker() as mock:
+        mock.get(api_url1, json=[], status_code=404)
+        with pytest.raises(reggov_api_doc_error.BadDocIDException):
+            documents_ids.get_documents_ids(API_KEY, dktid2)
 
-    assert resp[0] == []
 
-
-def test_status_code_two_hundred(requests_mock):
+def test_status_code_two_hundred():
     return_json = {'documents': [{'documentId': 'EDA-MP-OAR-2017-2244-1199'}]}
-    requests_mock.get(API_URL, json=return_json, status_code=200)
-
+    with requests_mock.Mocker() as mock:
+        mock.get(API_URL, json=return_json, status_code=200)
     # getting list of documents IDS
-    resp = documents_ids.get_documents_ids(API_KEY, DKT_ID)
-    assert resp[1] == 200
+        resp = documents_ids.get_documents_ids(API_KEY, DKT_ID)
+        assert resp[1] == 200
 
 
-def test_bad_api_key(requests_mock):
+def test_bad_api_key():
     return_json = {'documents': [{'documentId': 'FOP-RM-SDF-2014-3344-9988'}]}
     api_url = "{}?api_key={}s&dktid={}".format(URL, API_KEY, DKT_ID)
-    requests_mock.get(api_url, json=return_json, status_code=403)
-
-    # bad url test return 403
-    resp = documents_ids.get_documents_ids(str(API_KEY) + 's', DKT_ID)
-
-    assert resp[1] == 403
+    with requests_mock.Mocker() as mock:
+        mock.get(api_url, json=return_json, status_code=403)
+        with pytest.raises(reggov_api_doc_error.IncorrectApiKeyException):
+            documents_ids.get_documents_ids(str(API_KEY) + "s", DKT_ID)
