2eabe6ac197fb22e34bd187b0205f8068853ab9c
riyadgh <riyad.progs@gmail.com>
2020-03-13 16:08:35 -0400

    Fix typo

--- tests/test_getDocumentIDs.py
+++ tests/test_getDocumentIDs.py
@@ -1,10 +1,11 @@
 from c20_server import documentsIDs
 from dotenv import load_dotenv
 import os
-import requests
-import pytest
-import requests_mock
-import json
+
+# import requests
+# import pytest
+# import requests_mock
+# import json
 
 
 URL = "https://api.data.gov:443/regulations/v3/documents.json"
@@ -12,12 +13,14 @@ load_dotenv()
 api_key = os.getenv("KEY")
 dktid = "EPA-HQ-OAR-2011-0028"
 
-api_url = "{}?api_key={}&dktid={}".format(URL,api_key,dktid)
+api_url = "{}?api_key={}&dktid={}".format(URL, api_key, dktid)
 
 
 def test_get_response(requests_mock):
-
-    requests_mock.get(api_url, json = {'documents': [{'documentId': 'EPA-HQ-OAR-2011-0028-0041' }]}, status_code=200)
+    return_json = {'documents': [{'documentId': 'EPA-HQ-OAR-2011-0028-0041'}]}
+    requests_mock.get(api_url,
+                      json=return_json,
+                      status_code=200)
 
     # getting list of documents IDS
     resp = documentsIDs.get_documents_ids(api_key, dktid)
@@ -27,8 +30,8 @@ def test_get_response(requests_mock):
 
 def test_bad_docketID(requests_mock):
     dktid2 = 'EPA-HQ-0000'
-    api_url1 = "{}?api_key={}&dktid={}".format(URL,api_key,dktid2)
-    requests_mock.get(api_url1, json = [], status_code=200)
+    api_url1 = "{}?api_key={}&dktid={}".format(URL, api_key, dktid2)
+    requests_mock.get(api_url1, json=[], status_code=200)
 
     # getting empty data list
     resp = documentsIDs.get_documents_ids(api_key, dktid2)
@@ -37,8 +40,8 @@ def test_bad_docketID(requests_mock):
 
 
 def test_status_code_2OO(requests_mock):
-
-    requests_mock.get(api_url, json = {'documents': [{'documentId': 'EDA-MP-OAR-2017-2244-1199' }]}, status_code=200)
+    return_json = {'documents': [{'documentId': 'EDA-MP-OAR-2017-2244-1199'}]}
+    requests_mock.get(api_url, json=return_json, status_code=200)
 
     # getting list of documents IDS
     resp = documentsIDs.get_documents_ids(api_key, dktid)
@@ -47,11 +50,11 @@ def test_status_code_2OO(requests_mock):
 
 
 def test_bad_apikey(requests_mock):
-
-    api_url = "{}?api_key={}s&dktid={}".format(URL,api_key,dktid)
-    requests_mock.get(api_url, json = {'documents': [{'documentId': 'FOP-RM-SDF-2014-3344-9988' }]}, status_code=403)
+    return_json = {'documents': [{'documentId': 'FOP-RM-SDF-2014-3344-9988'}]}
+    api_url = "{}?api_key={}s&dktid={}".format(URL, api_key, dktid)
+    requests_mock.get(api_url, json=return_json, status_code=403)
 
     # bad url test return 403
-    resp = documentsIDs.get_documents_ids(str(api_key)+'s', dktid)
+    resp = documentsIDs.get_documents_ids(str(api_key) + 's', dktid)
 
     assert resp[1] == 403
